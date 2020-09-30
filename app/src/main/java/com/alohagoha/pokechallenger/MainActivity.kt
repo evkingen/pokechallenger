462f31e34e413060acf6cb5f8bb7b389b4a1df8c
alohagoha <zlock14@gmail.com>
2020-09-30 11:03:52 +0300

    initial commit

---
+++ app/src/main/java/com/alohagoha/pokechallenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alohagoha.pokechallenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
